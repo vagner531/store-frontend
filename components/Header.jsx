e96ff0ff15e8105eebd4233cb8f66e460287289f
vagner santos <vagner131417@gmail.com>
2023-04-10 21:04:38 -0300

    feat: add logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Wrapper from "./Wrapper";
+import Link from "next/link";
 
 const Header = () => {
-  return (
-    <div className="text">Header</div>
-  )
-}
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showCatMenu, setshowCatMenu] = useState(false);
+  const [show, setShow] = useState("tranlate-y-0");
+  const [lastScrollY, setlastScrollY] = useState(0);
 
-export default Header
+  return (
+    <header className={`w-full h-[50px] md:h-[80px] bg-black flex items-center 
+    justify-between text-white z-20 sticky top-0 transition-transform duration-300 tranlate-y-0 ${show}`}>
+      <Wrapper>
+        <Link href="/">
+          <img src="./logo-nerd.svg" className="w-[100px]" />
+        </Link>
+      </Wrapper>
+    </header>
+  );
+};
+
+export default Header;
